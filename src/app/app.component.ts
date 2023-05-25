ec4dff98abe2e9ed9804ae19a1d18e9738065c13
Tara Ubovic <taraubovic@Taras-Laptop.local>
2023-05-25 13:45:54 +0200

    trying sth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Default</h1>`,
+  template: `<h1>Template-Line</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
